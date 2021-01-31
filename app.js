010ede76f28f9076365368c08a3caa273588e51f
Jafari Jason <eng.jafari.m@gmail.com>
2021-01-30 22:27:31 -0500

    OK

--- app.js
+++ app.js
@@ -12,13 +12,18 @@ const products = [{
     }
 ]
 
+let cart = {
+    items: [],
+    total: 0
+}
+
 const renderProducts = () => {
     const productDiv = document.querySelector('.products')
     productDiv.innerHTML = ''
 
     products.forEach((item, index) => {
-        productDiv.innerHTML += 
-        `<div class="product">
+        productDiv.innerHTML +=
+            `<div class="product">
             <div class="product__image">
                 <img src="${item.image}" />
             </div>
@@ -27,7 +32,42 @@ const renderProducts = () => {
             <button class="add-to-cart">افزودن به سبد خرید</button>
         </div>`
     })
-    console.log(productDiv);
+}
+
+
+const renderCartItems = () => {
+    const cartDiv = document.querySelector('.cart__items')
+
+    cartDiv.innerHTML = ''
+
+    const totalPriceEl = document.querySelector('.cart__total-price')
+
+    let totalPraice = 0
+
+    if(cart.items.length == 0 ){
+        cartDiv.innerHTML = 'محصولی در سبد خرید وجود ندارد'
+    }
+
+    cart.items.forEach((item, index)=>{
+        totalPraice =+ item.total
+
+        cartDiv.innerHTML += `
+              <div class="cart__item">
+                <div class="col-md-4">
+                  <button class="cart__item-remove">حذف</button>
+                </div>
+                <div class="col-md-4">
+                  <div class="qty">${item.qty}</div>
+                </div>
+                <div class="col-md-4">
+                  <h3 class="cart__item-title">${item.name}</h3>
+                </div>
+              </div>
+        `
+    })
+
+    totalPriceEl.innerHTML = `مجموع: ${totalPraice}  تومان`
 }
 
 renderProducts()
+renderCartItems()
